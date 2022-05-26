46ad468141985c96c62ca0ac24b804880c96c3c8
vishnugopinathv <vishnugopinathv@gmail.com>
2022-05-26 18:58:16 +0530

    assessment5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Higher Order Functions</h1>
+      <ul>
+        <li>A higher-order component (HOC) is an advanced technique in React for reusing component logic. </li>
+        <li> HOCs are not part of the React API,They are a pattern that emerges from React’s compositional nature.</li>
+        <li>a higher-order component is a function that takes a component and returns a new component.</li>
+      </ul>
+      <h3>
+      Reason to use Higher-Order component:
+      </h3>
+      <ul>
+        <li>Easy to handle</li>
+        <li>Get rid of copying the same logic in every component</li>
+      </ul>
+      <h1>Pure Components</h1>
+      <ul>
+        <li>Pure Components in React are the components which do not re-renders when the value of state and props has been updated with the same values</li>
+        <li>PureComponent Is Primarily Used for Performance Optimization.</li>
+        <li>We can use React.memo for transforming a component into pure component</li>
+      </ul>
     </div>
   );
 }
